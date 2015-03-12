56e9073a622fd961585d95a00b3cdc28a6e22a7b
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-03-12 18:15:03 +0200

    controller client side

---
+++ Licenta-Frontend/client/js/controller/validateUser.js
@@ -0,0 +1,46 @@
+/**
+ * Created by root on 3/10/15.
+ */
+
+/**
+ * Function that performs a basic validation of userName and password.
+ * Maybe this should be refactored to work with JQuery
+ *
+ * @param userName
+ * @param password
+ */
+define([], function() {
+    function ValidationModule() {}
+
+    ValidationModule.isValidUserName = function (userName) {
+
+        if (!userName) {
+            return false;
+        }
+        if (userName === '' || userName === ' ') {
+            return false;
+        }
+        if (userName.length <= 1) {
+            return false;
+        }
+        return true;
+    }
+
+    ValidationModule.isValidPassword = function (password) {
+        if (!password) {
+            return false;
+        }
+
+        if (password === '' || password === ' ') {
+            return false;
+        }
+
+        if (password.length <= 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    return ValidationModule;
+});
