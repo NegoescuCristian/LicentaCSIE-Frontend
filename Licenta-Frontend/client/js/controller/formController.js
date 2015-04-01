e06decbb184f9b9e40da067e7af0b7e53a9c68ba
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-04-01 23:06:59 +0300

    modified .html pages plus the formController

--- Licenta-Frontend/client/js/controller/formController.js
+++ Licenta-Frontend/client/js/controller/formController.js
@@ -1,3 +1,15 @@
 /**
  * Created by root on 3/30/15.
  */
+define([], function () {
+
+    function FormController() {
+
+    }
+
+    FormController.prototype.start = function() {
+
+    }
+
+    return FormController;
+});
