aad9c5a0ca346d719ad8bacd4912caac548d761a
HurmuzacheCiprian <hurmuzache.ciprian@gmail.com>
2015-04-02 12:09:53 +0300

    moved default port to 8081

--- Licenta-Frontend/server/server.js
+++ Licenta-Frontend/server/server.js
@@ -10,7 +10,7 @@ var SessionStore = require('./lib/session-store/store');
 
 function start() {
     var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-    var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    var port = process.env.OPENSHIFT_NODEJS_PORT || 8081;
     console.log(ipaddress, port);
     http.createServer(function(request, response) {
         handleRequest(request, response);
